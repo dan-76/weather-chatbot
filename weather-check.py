570b195522e4f29696dea86069c3252d26c21ece
dan-76 <dc29183@connect.hku.hk>
2019-03-03 14:11:25 +0800

    Update weather-check.py
    add new data to scrape

--- weather-check.py
+++ weather-check.py
@@ -42,35 +42,44 @@ class current_weather:
         lang -- language
         author
         pub_date -- publish date
-        warning_msg -- weather warning message
+        weather_img_no -- image icon number for the weather summary
         temp -- tempature
         rel_humidity -- relative humidity
         uv_index -- UV index of past 1 hour
         uv_level -- UV level of past 1 hour
+        predict -- HKO special prediction
+        warning_msg -- weather warning message
         """
         html_soup = self.get_soup_from_url(self.url)
         des_item = self.get_soup_for_cdata(html_soup)
-        des_text = des_item.get_text()
+        self.des_text = des_item.get_text()
         self.status = html.status
         self.lang = html_soup.language.text
         self.author = html_soup.author.text
         pub_date_text = html_soup.pubdate.text
         self.pub_date = dateutil.parser.parse(pub_date_text)
-
-        self.temp = self.search_or_empty(r'Air temperature.* (\d+).*degrees Celsius', des_text)
-        self.rel_humidity = self.search_or_empty(r'Relative Humidity.* (\d+).*per cent', des_text)
-        self.uv_index = self.search_or_empty(r"the mean UV Index recorded at King's Park : (\d+.\d+)", des_text)
-        self.uv_level = self.search_or_empty(r'Intensity of UV radiation : (.*) ', des_text)
+        weather_img_url = des_item.find('img')['src']
+        self.weather_img_no = search_or_empty(r'http://rss.weather.gov.hk/img/pic(\d+).png', weather_img_url)
+        self.temp = self.search_or_empty(r'Air temperature.* (\d+).*degrees Celsius', self.des_text)
+        self.rel_humidity = self.search_or_empty(r'Relative Humidity.* (\d+).*per cent', self.des_text)
+        self.uv_index = self.search_or_empty(r"the mean UV Index recorded at King's Park : (\d+.\d+)", self.des_text)
+        self.uv_level = self.search_or_empty(r'Intensity of UV radiation : (\S*) ', self.des_text)
 
         #Warning in Chinese Source
         html_soup_uc = self.get_soup_from_url(self.url_uc)
         des_item_uc = self.get_soup_for_cdata(html_soup_uc)
+        self.predict = search_or_empty(u'(預 料 .*)', des_item_uc)
         des_text_warning_item = des_item_uc.find('span', {'id':'warning_message'})
         if des_text_warning_item is None:
             self.warning_msg = ""
         else:
             self.warning_msg = des_text_warning_item.text
-    
+
+        self.rainfall_exist = search_or_empty(r'(.*the rainfall recorded in various regions were.*)', des_text)
+        if self.rainfall_exist != '':
+            rainfall_table = des_item.find_all('table')[1]
+            self.rainfall_data = [x.text for x in rainfall_table.find_all('tr')]
+
     def store_scrape_result(self):
         pass
 
@@ -89,6 +98,11 @@ class current_weather:
             if self.warning_msg !='':
                 result += dedent(f"""
                 {self.warning_msg}""")
+
+            if self.predict !='':
+                result += dedent(f"""
+                {self.predict}""")
+
             return result
         else:
             return ''
