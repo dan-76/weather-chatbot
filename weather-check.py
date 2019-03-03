44d8d262943cd9e545280a9c1483533263f5851d
dan-76 <dc29183@connect.hku.hk>
2019-03-03 15:04:23 +0800

    Add telegram feature and bug fix

--- weather-check.py
+++ weather-check.py
@@ -14,6 +14,14 @@ import re
 import unittest
 from textwrap import dedent
 
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+from textwrap import dedent
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
 
 class current_weather:
     url = 'http://rss.weather.gov.hk/rss/CurrentWeather.xml' 
@@ -33,7 +41,7 @@ class current_weather:
         return BeautifulSoup(page, 'html.parser')
     
     def get_soup_for_cdata(self, htmlsoup):
-        des_html = html_soup.find(text=lambda tag: isinstance(tag, CData)).string.strip()
+        des_html = htmlsoup.find(text=lambda tag: isinstance(tag, CData)).string.strip()
         return BeautifulSoup(des_html, 'html.parser')
 
     def get_rss_data(self):
@@ -53,62 +61,95 @@ class current_weather:
         html_soup = self.get_soup_from_url(self.url)
         des_item = self.get_soup_for_cdata(html_soup)
         self.des_text = des_item.get_text()
-        self.status = html.status
         self.lang = html_soup.language.text
         self.author = html_soup.author.text
         pub_date_text = html_soup.pubdate.text
         self.pub_date = dateutil.parser.parse(pub_date_text)
         weather_img_url = des_item.find('img')['src']
-        self.weather_img_no = search_or_empty(r'http://rss.weather.gov.hk/img/pic(\d+).png', weather_img_url)
+        self.weather_img_no = self.search_or_empty(r'http://rss.weather.gov.hk/img/pic(\d+).png', weather_img_url)
         self.temp = self.search_or_empty(r'Air temperature.* (\d+).*degrees Celsius', self.des_text)
         self.rel_humidity = self.search_or_empty(r'Relative Humidity.* (\d+).*per cent', self.des_text)
-        self.uv_index = self.search_or_empty(r"the mean UV Index recorded at King's Park : (\d+.\d+)", self.des_text)
+        self.uv_index = self.search_or_empty(r"the mean UV Index recorded at King's Park.* (\d+)", self.des_text)
         self.uv_level = self.search_or_empty(r'Intensity of UV radiation : (\S*) ', self.des_text)
+        self.rainfall_exist = self.search_or_empty(r'(.*the rainfall recorded in various regions were.*)', self.des_text)
+        if self.rainfall_exist != '':
+            rainfall_table = des_item.find_all('table')[1]
+            self.rainfall_data = [x.text for x in rainfall_table.find_all('tr')]
 
         #Warning in Chinese Source
         html_soup_uc = self.get_soup_from_url(self.url_uc)
         des_item_uc = self.get_soup_for_cdata(html_soup_uc)
-        self.predict = search_or_empty(u'(預 料 .*)', des_item_uc)
+        self.predict = self.search_or_empty(u'(預 料 .*)', des_item_uc.get_text())
         des_text_warning_item = des_item_uc.find('span', {'id':'warning_message'})
         if des_text_warning_item is None:
             self.warning_msg = ""
         else:
             self.warning_msg = des_text_warning_item.text
 
-        self.rainfall_exist = search_or_empty(r'(.*the rainfall recorded in various regions were.*)', des_text)
-        if self.rainfall_exist != '':
-            rainfall_table = des_item.find_all('table')[1]
-            self.rainfall_data = [x.text for x in rainfall_table.find_all('tr')]
-
     def store_scrape_result(self):
         pass
 
     def scrape_result(self):
-        if self.status == 200:
-            result = dedent(f"""
-            時間: {self.pub_date.astimezone(tz.tzlocal()):%Y-%m-%d %H:%M:%S}
-            氣溫: {self.temp} 度
-            相對濕度: 百分之 {self.rel_humidity}""")
+        result = dedent(f"""
+        時間: {self.pub_date.astimezone(tz.tzlocal()):%Y-%m-%d %H:%M:%S}
+        氣溫: {self.temp} 度
+        相對濕度: 百分之 {self.rel_humidity}""")
 
-            if self.uv_index != '':
-                result += dedent(f"""
-                紫外線指數: {self.uv_index}
-                曝曬級數: {self.uv_level}""")
+        if self.uv_index != '':
+            result += dedent(f"""
+            紫外線指數: {self.uv_index}
+            曝曬級數: {self.uv_level}""")
 
-            if self.warning_msg !='':
-                result += dedent(f"""
-                {self.warning_msg}""")
+        if self.warning_msg !='':
+            result += dedent(f"""
+            {self.warning_msg}""")
 
-            if self.predict !='':
-                result += dedent(f"""
-                {self.predict}""")
+        if self.predict !='':
+            result += dedent(f"""
+            {self.predict}""")
 
-            return result
-        else:
-            return ''
+        return result
+
+
+class weathertelebot:
+    def __init__(self, tgToken):
+        self.token = tgToken
+    
+    def start_def_bot(self):
+        self.updater = Updater(token=self.token)
+        dispatcher = self.updater.dispatcher
+
+        def start(bot, update):
+            bot.send_message(chat_id=update.message.chat_id, 
+            text=dedent("""
+            You can control me by sending these commands:
+            
+            /weather - get the current weather report"""))
+
+        start_handler = CommandHandler('start', start)
+        dispatcher.add_handler(start_handler)
+
+        def get_weather(bot, update):
+            c1 = current_weather()
+            c1.get_rss_data()
+            bot.send_message(chat_id=update.message.chat_id, text=c1.scrape_result())
+            del c1
+
+        get_weather_handler = CommandHandler('weather', get_weather)
+        dispatcher.add_handler(get_weather_handler)
+
+    def start_bot_host(self):
+        self.updater.start_polling()
 
 
 if __name__ == '__main__':
-    c1 = current_weather()
-    c1.get_rss_data()
-    print(c1.scrape_result())
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Host the telegram bot of weather')
+    parser.add_argument('tgToken', metavar='string token',
+                        help='This is the token for bot')
+    args = parser.parse_args()
+
+    t1 = weathertelebot(args.tgToken)
+    t1.start_def_bot()
+    t1.start_bot_host()
